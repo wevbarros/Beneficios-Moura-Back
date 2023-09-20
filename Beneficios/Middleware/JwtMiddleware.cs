2c1a235ce23d9debef8682c176aa8505e7fd62e6
sudown <mirosmarsantoss@gmail.com>
2023-09-20 17:37:13 -0300

    Bloqueando post, put e delete sem autenticação

--- Beneficios/Middleware/JwtMiddleware.cs
+++ Beneficios/Middleware/JwtMiddleware.cs
@@ -11,46 +11,47 @@ public class JwtMiddleware
   }
 
   public async Task InvokeAsync(HttpContext context)
-  {
+{
     var path = context.Request.Path;
+    var method = context.Request.Method;
 
-    if (path.StartsWithSegments("/login"))
+    if (path.StartsWithSegments("/login") || path.StartsWithSegments("/") || path.StartsWithSegments("/beneficios") && method == HttpMethods.Get)
     {
-      await _next(context); // Skip token validation for /login and /refreshToken endpoints
-      return;
+        await _next(context); // Skip token validation for /login and /refreshToken endpoints
+        return;
     }
 
-    if (context.Request.Headers.ContainsKey("Authorization"))
+    if (method != HttpMethods.Get && !context.Request.Headers.ContainsKey("Authorization"))
     {
-      var authorizationHeader = context.Request.Headers["Authorization"].ToString();
-      var token = authorizationHeader.Replace("Bearer ", string.Empty);
-
-      if (string.IsNullOrWhiteSpace(token))
-      {
         context.Response.StatusCode = 401;
         await context.Response.WriteAsync("Unauthorized");
-      }
-      else
-      {
-        // Validate the token
+        return;
+    }
+
+    var authorizationHeader = context.Request.Headers["Authorization"].ToString();
+    var token = authorizationHeader.Replace("Bearer ", string.Empty);
+
+    if (string.IsNullOrWhiteSpace(token))
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Unauthorized");
+    }
+    else
+    {
+        // Valide o token JWT
         bool isValid = JWT.ValidateToken(token);
 
         if (!isValid)
         {
-          context.Response.StatusCode = 401;
-          await context.Response.WriteAsync("Unauthorized");
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Unauthorized");
         }
         else
         {
-          context.Items["JwtToken"] = token;
-          await _next(context);
+            context.Items["JwtToken"] = token;
+            await _next(context);
         }
-      }
     }
-    else
-    {
-      context.Response.StatusCode = 401;
-      await context.Response.WriteAsync("Unauthorized");
-    }
-  }
+}
+
 }
